307b62a9ff9016de3de0c835aff95c6462eee43d
vinm0 <68492272+vinm0@users.noreply.github.com>
2021-05-27 14:07:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
     
+    // validate principal
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+
+    // Calculate current year + interest years
+    var year = new Date().getFullYear()+parseInt(years);
+
+    var result = document.getElementById("result");
+
+    // display summary of calculation result
+    result.innerHTML = `\<br\>\<br\>
+If you deposit <mark>$${principal}</mark>,\<br\>
+at an interest rate of <mark>${rate}% </mark>.\<br\>
+You will receive an amount of <mark>$${interest}</mark>,\<br\>
+in the year <mark>${year}</mark> \<br\>`;
 }
-        
+
+
+function updateRate() 
+{
+    // update value of percentage range
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
+
